044f0ead4acbfb53b5e606bf2204e86d2c3d0312
Roar Lochar <roar.lochar@tietoevry.com>
2023-02-15 12:56:45 +0100

    Changed implementation of ViewModel to conform to ObservableObject and create a published variable.

--- Albertos/Albertos/ViewModels/MenuListViewModel.swift
+++ Albertos/Albertos/ViewModels/MenuListViewModel.swift
@@ -4,19 +4,12 @@
 //
 //  Created by Roar Lochar on 14/02/2023.
 //
+import Combine
 
-public struct MenuListViewModel {
-    private let menu: [MenuItem]
-    private let grouping: ([MenuItem]) -> [MenuSection]
-    
-    public var sections: [MenuSection]  {
-        get {
-            grouping(menu)
-        }
-    }
+public class MenuListViewModel: ObservableObject {
+    @Published public private(set) var sections: [MenuSection]
     
     public init(menu: [MenuItem], menuGrouping: @escaping ([MenuItem]) -> [MenuSection] = groupMenuByCategory) {
-        self.menu = []
-        self.grouping = menuGrouping
+		sections = menuGrouping([])
     }
 }
